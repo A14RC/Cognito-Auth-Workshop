710d2d4ac40931a947bab1a049a35bf0326510a1
andres14romero <desarrollo4@corsinf.com>
2026-02-02 20:07:59 -0500

    auth_2 agrego acceso a rol manager

--- src/main/kotlin/com/pucetec/auth/controllers/DummyController.kt
+++ src/main/kotlin/com/pucetec/auth/controllers/DummyController.kt
@@ -2,7 +2,6 @@ package com.pucetec.auth.controllers
 
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,39 +11,37 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class DummyController {
 
-    // A este endpoint cualquiera puede acceder
-    //ejemplo: healt, mostrar catalogos que no sean llenos de datos sensibles
-
-
     @GetMapping("/public")
     fun public(): Response {
-        return Response ("Todo ok. Este es un endpoint publico")
+        return Response("Todo ok. Este es un endpoint publico")
     }
-
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/restricted")
     fun restricted(
         @AuthenticationPrincipal jwt: Jwt?,
-    ):Response {
+    ): Response {
         val user = jwt?.getClaim<String>("username") ?: "usuario"
         return Response("Todo Ok. Este es un endpoint para usuarios autenticados. Hola ${user}")
     }
 
-    // A este endpoint solo usuarios que hayan iniciado sesion y tengan el rol de administrador
-    // pueden acceder, manipular configuraciones para cualquier accion que requiera privilegios.
     @PreAuthorize("hasAuthority('ROLE_admin')")
     @GetMapping("/restricted/admin")
-    fun admin():Response {
+    fun admin(): Response {
         return Response("Todo Ok. Este es un endpoint solo para admins")
     }
 
     @PreAuthorize("hasAnyRole('ROLE_admin', 'ROLE_manager')")
     @GetMapping("/restricted/sensitive")
-    fun sensitiveData():Response {
+    fun sensitiveData(): Response {
         return Response("Todo Ok. Este es un endpoint solo para admins y managers")
     }
 
-
+    @PreAuthorize("hasAnyRole('ROLE_manager')")
+    @GetMapping("/restricted/manager")
+    fun managerOnly(): Response {
+        return Response("Todo Ok. Este es un endpoint solo managers")
+    }
 }
-data class Response(val mesagge: String)
+
+data class Response(val message: String)
